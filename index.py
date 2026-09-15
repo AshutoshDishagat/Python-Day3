117b91636eea9089f8fac0f2316898570b2a3ad9
AshutoshDishagat <dishagatashutosh@gmail.com>
2026-09-15 09:53:50 +0530

    Second Code

--- index.py
+++ index.py
@@ -1,3 +1,20 @@
+# While loop example
+
 while True:
 		string = input("Enter a string: ")
 		print(string)
+
+
+
+# 2nd Code 
+
+correct_pass = "Some_pass"
+not_found = True
+
+while not_found:
+    user_pass = input("Enter the password: ")
+    if user_pass == correct_pass:
+        print("Access granted")
+        not_found = False
+
+print("Password Matched!")    
