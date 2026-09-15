13dc5de14c4e175b6cda4a5a1efabe3dd82e5d40
AshutoshDishagat <dishagatashutosh@gmail.com>
2026-09-15 10:19:55 +0530

    5th Code

--- index.py
+++ index.py
@@ -26,3 +26,35 @@ num = int(input("Enter a number to print its table: "))
 while i <= 10: 
         print(num * i)
         i += 1
+
+
+# 4th Code - Break statement example
+
+correct_pass = "Some_pass"
+not_found = True
+
+while not_found:
+    user_pass = input("Enter the password: ")
+    if user_pass == correct_pass:
+        break
+
+
+# 5th Code - Buggy code - infinite loop
+
+i = 0
+while i < 10:
+       if i == 5:
+           continue
+       print(i)
+       i += 1
+
+# Solution: 
+i = 0
+while i < 10:
+       if i == 5:
+           i += 1
+           continue
+       print(i)
+       i += 1
+       
+    
