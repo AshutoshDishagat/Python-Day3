0ce857f1ffb3007a782466206355b6d0d721f4c0
AshutoshDishagat <dishagatashutosh@gmail.com>
2026-09-15 10:34:57 +0530

    7th Code

--- 2)for_loop.py
+++
@@ -1,5 +0,0 @@
-# 6th Code - For loop example
-
-l = [1, 2, 3, 4, 5] # python list
-for i in l:
-    print(i) 
---
+++ for_loop2.py
@@ -0,0 +1,9 @@
+# 6th Code - For loop example
+
+l = [1, 2, 3, 4, 5] # python list
+for i in l:
+    print(i) 
+
+# 7th Code - For loop with range
+for i in range(1, 6):
+    print(i) 
