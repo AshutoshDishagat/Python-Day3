41abf642af7a1fa5253f0ce88b8fa21ac92698c3
AshutoshDishagat <dishagatashutosh@gmail.com>
2026-09-15 10:37:26 +0530

    7th Code

--- for_loop2.py
+++ for_loop2.py
@@ -4,6 +4,6 @@ l = [1, 2, 3, 4, 5] # python list
 for i in l:
     print(i) 
 
-# 7th Code - For loop with range
+# 7th Code - For loop with range ->  it will print till 5 ( exclusive )
 for i in range(1, 6):
     print(i) 
